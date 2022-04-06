88ec4ca5ab1fe844d23bb1d2e174d94ff1b2df34
HomelessGodYato <yaroslavbanchenko@gmail.com>
2022-04-06 20:40:52 +0200

    Green rectangle around every hand

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,6 +1,7 @@
+import time
+
 import cv2
 import mediapipe as mp
-import time
 
 previous_Time = 0
 current_time = 0
@@ -16,21 +17,39 @@ mp_draw = mp.solutions.drawing_utils
 mHands = mp.solutions.hands
 hands = mHands.Hands()
 
-
+_, frame = capture.read()
+h, w, c = frame.shape
 
 while True:
     success, frame = capture.read()
     flip = cv2.flip(frame, 1)
     frame_RGB = cv2.cvtColor(flip, cv2.COLOR_BGR2RGB)
     result = hands.process(frame_RGB)
+    hand_lm = result.multi_hand_landmarks
+
     # print(result.multi_hand_landmarks)
 
-    if result.multi_hand_landmarks:
-        for hand_landmarks in result.multi_hand_landmarks:
-            mp_draw.draw_landmarks(flip, hand_landmarks,mHands.HAND_CONNECTIONS)
+    if hand_lm:
+        for hand_landmarks in hand_lm:
+            x_max = 0
+            y_max = 0
+            x_min = w
+            y_min = h
+            for lm in hand_landmarks.landmark:
+                x, y = int(lm.x * w), int(lm.y * h)
+                if x > x_max:
+                    x_max = x
+                if y > y_max:
+                    y_max = y
+                if x < x_min:
+                    x_min = x
+                if y < y_min:
+                    y_min = y
+            cv2.rectangle(flip, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+            mp_draw.draw_landmarks(flip, hand_landmarks, mHands.HAND_CONNECTIONS)
 
     current_time = time.time()
-    fps = 1/(current_time-previous_Time)
+    fps = 1 / (current_time - previous_Time)
     previous_Time = current_time
 
     cv2.putText(flip, "FPS: " + str(int(fps)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
